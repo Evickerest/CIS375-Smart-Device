2af00d266068a6ca9d97b618b057ab0b7cc0f64a
Evickerest <ejmoras17@gmail.com>
2025-03-25 22:05:32 -0400

    Added docs directory, added basic pdf reading

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CIS375-Smart-Device
+
+## Set up
+To install the necessary dependencies, ensure that pip is installed and run this command on the terminal:
+```bash
+pip install -r requirements.txt
+```
---
+++ program.py
@@ -0,0 +1,31 @@
+# This package allows us load PDF documents
+from langchain_community.document_loaders import PyPDFLoader
+import glob
+import os
+
+# File path of where our documents are stored
+DOCS_PATH = "./docs/"
+
+# This will create a list of all the files under the ./docs/ directory that are pdfs
+file_paths = glob.glob(f"{DOCS_PATH}*.pdf")
+
+# Empty list to store our pdfs in
+pdfs = []
+
+# Loop through all the files
+for file_path in file_paths:
+    # Using the PyPDFLoader package, this will load the PDF as an object 
+    pages = PyPDFLoader(file_path).load()
+
+    # Add our pages to our pdfs list
+    pdfs.append(pages)
+
+# Loop through all the pdfs
+for pdf in pdfs:
+    # Each pdf contains multiple pages
+    for page in pdf:
+        # A "page" object contains multiple fields: author, date, size, etc. The actual content of the page is in the "page_content" field
+        print(f"Page Content: {page.page_content}\n")
+
+
+
