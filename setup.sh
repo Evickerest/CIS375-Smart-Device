a84226225d07caccb5cab7f6ef272140b2c0fea3
Evickerest <ejmoras17@gmail.com>
2025-04-12 18:36:10 -0400

    Cleaned up files

--- setup.sh
+++ setup.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-echo Setting up...
+echo Beginning setting up...
+echo Installing python requirments...
 pip install -r requirements.txt
+
+echo Installing Ollama...
 curl -fsSL https://ollama.com/install.sh | sh
-ollama pull llama3-chatqa:8b
+
+echo Pulling Ollama Model...
+ollama pull llama3:8b
+
+echo Creating Vector DB from PDFs
+python3 pdf_read.py
+
+echo Training AI on AWID CSVs
+python3 train_ai.py
+
+echo To use, run "chat_ai.py", and then run "packet_sniffer.py" to begin.
 echo Done!
